766d0941b22e027f68b6aaa26176e96a0827d4df
Tom2529 <tomkovoorjava7@gmail.com>
2026-01-31 15:26:06 -0800

    Add corrected version of main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,8 +13,18 @@ provider "aws" {
 }
 
 resource "aws_instance" "usermgmtbe" {
-  ami           = "ami-0290e60ec230db1e4"  # Amazon Linux 2 in us-east-1, update per your region
+  ami = "ami-0dba2cb6798deb6d8"  
   instance_type = "t3.medium"
+  
+  user_data = <<-EOF
+  		#!/bin/bash
+                sudo apt update -y
+		sudo apt install openjdk-21-jdk -y
+		sudo apt install maven -y
+		sudo apt install git -y
+		git clone -b security https://github.com/neerajbalodi/user-management-backend.git /home/ubuntu/user-management-backend 
+		chown -R ubuntu:ubuntu /home/ubuntu/user-management-backend
+              EOF
 
   tags = {
     Name = "usermgmtbe"
