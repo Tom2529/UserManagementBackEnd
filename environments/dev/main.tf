a06c6dde03d54556ad31d520403caddad10f8c20
Tom2529 <tomkovoorjava7@gmail.com>
2026-01-31 15:12:41 -0800

    Changes to EC instance

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,8 +13,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "usermgmtbe" {
-  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1, update per your region
-  instance_type = "t3.micro"
+  ami           = "ami-0290e60ec230db1e4"  # Amazon Linux 2 in us-east-1, update per your region
+  instance_type = "t3.medium"
 
   tags = {
     Name = "usermgmtbe"
