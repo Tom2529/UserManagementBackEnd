2fd7843d91b77ac1820c7a96e4850ac80305c336
Tom2529 <tomkovoorjava7@gmail.com>
2026-01-31 15:07:22 -0800

    updating region and instance name

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -9,14 +9,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"  # Change this if needed
+  region = "us-west-1"  # Change this if needed
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "usermgmtbe" {
   ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1, update per your region
   instance_type = "t3.micro"
 
   tags = {
-    Name = "terraform-example-instance"
+    Name = "usermgmtbe"
   }
 }
