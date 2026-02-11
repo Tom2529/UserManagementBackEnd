94a5192b94ee590cdb426a37b83f752d9e9ac3bc
Tom2529 <tomkovoorjava7@gmail.com>
2026-02-10 22:09:09 -0800

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,4 +1,19 @@
-variable "region" {
-  description = "AWS region"
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
+variable "security_group_name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+variable "ami" {
+  description = "Amazon machine id"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
   type        = string
 }
